371e4e34d8b4f293307dd38810c2c42ef866603c
Jack Skrable <jskrable@bu.edu>
2018-12-13 09:36:02 -0500

    write results to file

--- diff-debug.py
+++ diff-debug.py
@@ -4,15 +4,15 @@ import json
 
 
 def read_input():
-    with open('sub_modified-uids.json') as f:
+    with open('modified-uids.json') as f:
         mod = json.load(f)
         print('Reading mod file...')
 
-    with open('sub_old.json', encoding='latin-1') as f:
+    with open('old.json', encoding='latin-1') as f:
         old = json.load(f)
         print('Reading old file...')
 
-    with open('sub_new.json', encoding='latin-1') as f:
+    with open('new.json', encoding='latin-1') as f:
         new = json.load(f)
         print('Reading new file...')
 
@@ -27,6 +27,24 @@ def sample_set(data, size):
         subset.append(entry)
     return subset
 
+def output(mod):
+
+    if not mod:
+        return None
+    else:
+        with open('applicant_mod_results.json', 'a+') as f:
+            if f.tell() == 0:
+                a = []
+                a.append(mod)
+                json.dump(a, f, indent=2)
+            else:
+                f.seek(f.tell() - 1, os.SEEK_SET)
+                f.truncate()
+                f.write(',')
+                json.dump(mod, f, indent=2)
+                f.write(']')
+        
+
 def object_diff(new, old, mod, buid):
 
     def new_mod():
@@ -38,27 +56,30 @@ def object_diff(new, old, mod, buid):
                     key: old[key]
                     }
                 }
-        mod.append(d)
+        mod.update(d)
+    
 
-        
-    for key in new:
-        if new[key] != old[key]:
-            if len(mod) == 0:
-                new_mod()
-            for entry in mod:
-                if entry['buid'] == buid:
-                    entry['new'].update({key: new[key]}) 
-                    entry['old'].update({key: old[key]}) 
-                else:
+    try:
+        for key in new:
+            if new[key] != old[key]:
+                if not mod:
                     new_mod()
+                else:
+                    try: 
+                        mod['new'].update({key: new[key]})
+                        mod['old'].update({key: old[key]})
+                    except KeyError:
+                        new_mod()
+    except TypeError:
+        None
     return mod
         
 
 def diff(uids, new, old):
     print('Running diff...')
-    mod = []
 
     for i in uids:
+        mod = {}
         try:
             new_person = [x['applicant'] for x in new if x['applicant']['univId'] == i][0]
             old_person = [x['applicant'] for x in old if x['applicant']['univId'] == i][0]
@@ -67,13 +88,16 @@ def diff(uids, new, old):
             new_couns = [x['counselor'] for x in new if x['applicant']['univId'] == i][0]
             old_couns = [x['counselor'] for x in old if x['applicant']['univId'] == i][0]
         except IndexError:
-            mod.append({'buid': i,
+            mod.update({'buid': i,
                         'error': 'Insert/delete'
                         })
         
         mod = object_diff(new_person,old_person,mod,i)
         mod = object_diff(new_app,old_app,mod,i)
         mod = object_diff(new_couns,old_couns,mod,i)
+
+        output(mod)
+        mod = {}
 
     return mod
                 
@@ -88,9 +112,7 @@ def main():
     old = [x for x in old if x['applicant']['univId'] in uids]
     
 
-    changed = diff(uids, new, old)
-
-    print(changed)
+    diff(uids, new, old)
 
 if __name__ == '__main__':
     main()
